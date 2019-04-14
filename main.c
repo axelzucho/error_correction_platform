744d33435e2c4312665d264a3697703e41d4dd38
axelzucho <axelzucho1@gmail.com>
2019-04-14 12:14:29 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define STR_LEN 100
+
+void menu(){
+    char filename[STR_LEN];
+    int number_of_servers;
+
+    printf("Welcome to the error correction testing platform\n");
+    printf("Please enter the file you want to test with\n");
+
+    scanf("%s", filename);
+
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
